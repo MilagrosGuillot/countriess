6716d4dae6f4a95425ce5f8d8d1f759d2479b66a
Mili <101240663+MilagrosGuillot@users.noreply.github.com>
2024-03-16 20:56:50 -0300

    readme

--- piCountry/client/src/views/index.js
+++
@@ -1,6 +0,0 @@
-import LandingPage from "./LandingPage/LandingPage";
-import HomePage from "./HomePage/HomePage";
-import Form from "./Form/Form"
-import Detail from "./Detail/Detail"
-
-export {LandingPage, HomePage, Form, Detail}
